379a22d0b739333eacf0959fb5feee069a4fc012
Antonio Terrero <terrero@foxxum.com>
2021-10-02 11:29:27 +0200

    📄 lic: Set CC BY-NC-4 license

--- libs/api-interfaces/src/index.ts
+++ libs/api-interfaces/src/index.ts
@@ -1,1 +1,9 @@
+// Gemxploding (c) by ATAlgaba
+// 
+// Gemxploding is licensed under a
+// Creative Commons Attribution-NonCommercial 4.0 International License.
+// 
+// You should have received a copy of the license along with this
+// work. If not, see <http://creativecommons.org/licenses/by-nc/4.0/>.
+
 export * from './lib/api-interfaces';
--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,11 @@
+// Gemxploding (c) by ATAlgaba
+// 
+// Gemxploding is licensed under a
+// Creative Commons Attribution-NonCommercial 4.0 International License.
+// 
+// You should have received a copy of the license along with this
+// work. If not, see <http://creativecommons.org/licenses/by-nc/4.0/>.
+
 export interface Card {
   id: number;
   name: string;
