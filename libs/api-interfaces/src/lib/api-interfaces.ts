8cfda10a5e745bd6b5fc6ed69940535d6aa183d2
Antonio Terrero <terrero@foxxum.com>
2021-10-02 11:07:28 +0200

    🏷️ api: add basic interfaces for Game management

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,33 @@
-export interface Message {
-  message: string;
+export interface Card {
+  id: number;
+  name: string;
+}
+
+export interface GemCard extends Card {
+  value: number;
+}
+
+export interface SpecialCard extends Card {
+  onUse?: () => void;
+  onDraw?: () => void;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  color: string;
+}
+
+export interface Player extends User {
+  backpack: Array<Card>;
+  hand: Array<Card>;
+}
+
+export interface Game {
+  id: string;
+  password: string;
+  turn: number;
+  players: Array<Player>;
+  drawPile: Array<Card>;
+  discardPile: Array<Card>;
 }
